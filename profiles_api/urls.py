4d7e57abd933c1dc4b2e2e6be4c5b42b32917539
nazmul1487 <nazmul15-1487@diu.edu.bd>
2020-10-11 22:25:30 +0600

    api_view and view_set added

---
+++ profiles_api/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path, include
+
+from rest_framework.routers import DefaultRouter
+
+from .views import HelloApiView, HelloViewSet
+
+router = DefaultRouter()
+router.register('hello-viewset', HelloViewSet, basename='hello-viewset')
+
+
+
+urlpatterns = [
+    path('hello-view/', HelloApiView.as_view(), name='hello-view'),
+    path('', include(router.urls)),
+]
---
+++ profiles_api/views.py
@@ -0,0 +1,86 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework import viewsets
+# Create your views here.
+
+from.serializers import HelloSerializer
+
+
+class HelloApiView(APIView):
+
+    serializer_class = HelloSerializer
+
+    def get(self, request, format=None):
+
+        an_apiview = [
+            'Uses HTTP methods as function (get, post, patch, put, delete)',
+            'Is similar to a traditional Django View',
+            'Gives you the most control over you application logic',
+            'Is mapped manually to URLs',
+        ]
+        return Response({'message': 'Hello', 'an_apiview': an_apiview})
+
+    def post(self, request):
+
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = 'Hello {}'.format(name)
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+    def put(self, request, pk=None):
+        return Response({'method': 'PUT'})
+
+    def patch(self, request, pk=None):
+        return Response({'method': 'PATCH'})
+
+    def delete(self, request, pk=None):
+        return Response({'method': 'DELETE'})
+
+
+
+class HelloViewSet(viewsets.ViewSet):
+    serializer_class = HelloSerializer
+
+    def list(self, request):
+        a_viewset = [
+            'Uses actions (list, create, retrieve, update, partial_update)',
+            'Automatically maps to URLs using Routers',
+            'Provides more functionality with less code',
+
+        ]
+        return Response({'message': 'Hello', 'a_viewset': a_viewset})
+
+    def create(self, request):
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = 'Hello {}'.format(name)
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+    def retrieve(self, request, pk=None):
+        return Response({'http_method': 'GET'})
+
+    def update(self, request, pk=None):
+        return Response({'http_method': 'PUT'})
+
+    def partial_update(self, request, pk=None):
+        return Response({'http_method': 'PATCH'})
+
+    def destroy(self, request, pk=None):
+        return Response({'http_method': 'DELETE'})
+
